23060cab2e2f0d7e27b82a36406702192fe317a0
Jianhang Yin <yinjianhang@ou.edu>
2019-09-27 00:20:36 -0700

    09/27/2019

---
+++ Python3/linked_list/middle_of_the_linked_list.py
@@ -0,0 +1,30 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def middleNode(self, head: ListNode) -> ListNode:
+        slow_pointer = head
+        fast_pointer = head
+        while fast_pointer.next is not None:
+            slow_pointer = slow_pointer.next
+            fast_pointer = fast_pointer.next.next
+            if fast_pointer is None:
+                return slow_pointer
+        return slow_pointer
+
+
+node_list = []
+for i in range(6):
+    node_list.append(ListNode(i + 1))
+for i in range(6):
+    if i == 5:
+        node_list[i].next = None
+    else:
+        node_list[i].next = node_list[i + 1]
+s = Solution()
+result = s.middleNode(node_list[0])
+print(result.val)
---
+++ Python3/linked_list/reverse_linked_list.py
@@ -0,0 +1,29 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        if head is None or head.next is None:
+            return head
+        else:
+            reverse_head = self.reverseList(head.next)
+            head.next.next = head
+            head.next = None
+            return reverse_head
+
+
+node_list = []
+for i in range(6):
+    node_list.append(ListNode(i + 1))
+for i in range(6):
+    if i == 5:
+        node_list[i].next = None
+    else:
+        node_list[i].next = node_list[i + 1]
+s = Solution()
+result = s.reverseList(node_list[0])
+print(result.val)
